8ccce98c27ce929a10dabb70f8f4c21ecdb06440
Javier Moran Lemus <moranlemusj23@gmail.com>
2017-11-21 16:42:27 +0100

    email validation

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -5,7 +5,8 @@ import { IpfsService } from '../ipfs.service';
 
 import {DragZoneComponent } from '../dragzone/dragzone.component'
 
-const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)+$/;
+const MULTIPLE_REGEX = /^([a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)+(,+ )?)+$/;
 
 @Component({
   selector: 'app-input',
@@ -56,30 +57,36 @@ export class InputComponent {
 //Verifies email inputs
 toEmailFormControl = new FormControl('', [
   Validators.required,
+  Validators.pattern(MULTIPLE_REGEX)]);
+fromEmailFormControl = new FormControl('', [
+  Validators.required,
   Validators.pattern(EMAIL_REGEX)]);
-  fromEmailFormControl = new FormControl('', [
-    Validators.required,
-    Validators.pattern(EMAIL_REGEX)]);
 
 //Called when form is submitted
 onTestPost() {
-  if (this.file.length && this.data.to) {
-    this.form = false;
-    this.submit = true;
-    setTimeout(()=> {
-      this.submit = false;
-      this.submitResponse = true;
-    }, 2000);
-  this.emailService.sendEmail(this.data.to, this.data.from, this.data.hashes)
-  .subscribe(
-    data => {
-      this.postData = JSON.stringify(data),
-      console.log('POST', this.postData)},
-    error => console.log("Error 123", error)
-    );
-  }
+  console.log('REGEX!!!!!!!!!!!!!!!!!', this.data.to.match(MULTIPLE_REGEX))
+  if (!this.data.to.match(MULTIPLE_REGEX)) alert (`Invalid Recipient, please verify recpient's email!`);
+  else if (!this.data.from.match(EMAIL_REGEX)) alert (`Invalid Sender, please verify senders's email!`);
   else {
-    alert ("No file selected");
+    if (this.file.length && this.data.to) {
+      console.log(this.data.to);
+      this.form = false;
+      this.submit = true;
+      setTimeout(()=> {
+        this.submit = false;
+        this.submitResponse = true;
+      }, 2000);
+    this.emailService.sendEmail(this.data.to, this.data.from, this.data.hashes)
+    .subscribe(
+      data => {
+        this.postData = JSON.stringify(data),
+        console.log('POST', this.postData)},
+      error => console.log("Error 123", error)
+      );
+    }
+    else {
+      alert ("No file selected");
+    }
   }
 }
 
