384413fc27063e6415cdd8ac3ce4c5a43c9e5bce
Alex Sicart Ramos <hello@alexsicart.com>
2018-01-26 14:51:35 +0100

    fixes

--- styles.bundle.js
+++ styles.bundle.js
@@ -393,7 +393,7 @@ if(false) {
 
 /***/ }),
 
-/***/ 31:
+/***/ 32:
 /***/ (function(module, exports, __webpack_require__) {
 
 module.exports = __webpack_require__("./src/styles.css");
@@ -401,5 +401,5 @@ module.exports = __webpack_require__("./src/styles.css");
 
 /***/ })
 
-},[31]);
+},[32]);
 //# sourceMappingURL=styles.bundle.js.map
