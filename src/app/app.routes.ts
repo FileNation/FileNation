ee866966762954271f98e6b69fc53a2e10006f50
Alex Sicart Ramos <hello@alexsicart.com>
2018-03-11 14:06:24 +0100

    v-0.1

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import {Routes} from '@angular/router'
-
-import {HomeComponent} from './home/home.component'
-
-export const ROUTES: Routes = [
-    {path: '', component: HomeComponent},
-    // {path: 'about', loadChildren: './about/about.module#AboutModule'}
-
-]
